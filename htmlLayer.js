6afa837fa87275eebef9432a8803f717b4f8c5ef
IanHern <IanHern@gmail.com>
2015-04-20 20:35:58 +1000

    Sky changes colour as you go up.

--- htmlLayer.js
+++ htmlLayer.js
@@ -1661,8 +1661,9 @@ function approach(start, destination, rate)
 
 function draw()
 {
-    // Fill to black.
-    canvasContext.fillStyle = "#05b9db";
+    var fillColour = "#05b9db";
+    var fillRate = -((playerSpawn.y - player.position.y)/playerSpawn.y);
+    canvasContext.fillStyle = shadeBlend(fillRate, fillColour, "#000000");
     canvasContext.fillRect(0,0,canvas.width,canvas.height);
 
     var cameraOffset = v2Multiply(camera.target.position, camera.scale);
@@ -2187,6 +2188,7 @@ function loadMap(mapJson)
 //
 //====== MATH ======
 //
+
 function v2(x, y)
 {
     if(isNaN(x))
@@ -2285,6 +2287,26 @@ function v2Copy(v)
 function angleToV2(a)
 {
     return new v2(-Math.cos(a), -Math.sin(a));
+}
+
+//
+//====== COLOUR ======
+//
+
+//http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+function shadeBlend(p,c0,c1)
+{
+    var n=p<0?p*-1:p,u=Math.round,w=parseInt;
+    if(c0.length>7)
+    {
+        var f=c0.split(","),t=(c1?c1:p<0?"rgb(0,0,0)":"rgb(255,255,255)").split(","),R=w(f[0].slice(4)),G=w(f[1]),B=w(f[2]);
+        return "rgb("+(u((w(t[0].slice(4))-R)*n)+R)+","+(u((w(t[1])-G)*n)+G)+","+(u((w(t[2])-B)*n)+B)+")"
+    }
+    else
+    {
+        var f=w(c0.slice(1),16),t=w((c1?c1:p<0?"#000000":"#FFFFFF").slice(1),16),R1=f>>16,G1=f>>8&0x00FF,B1=f&0x0000FF;
+        return "#"+(0x1000000+(u(((t>>16)-R1)*n)+R1)*0x10000+(u(((t>>8&0x00FF)-G1)*n)+G1)*0x100+(u(((t&0x0000FF)-B1)*n)+B1)).toString(16).slice(1)
+    }
 }
 
 /*
